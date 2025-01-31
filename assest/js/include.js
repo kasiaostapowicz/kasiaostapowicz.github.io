694fe60fae58328776d0d4cf39028c2c17698276
Katarzyna <78623033+kasiaostapowicz@users.noreply.github.com>
2025-01-31 10:04:34 +0100

    Update include.js

--- assest/js/include.js
+++ assest/js/include.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const includes = document.querySelectorAll('[data-include]');
+    includes.forEach(async function(el) {
+        const file = el.getAttribute('data-include');
+        try {
+            const response = await fetch(file);
+            if (response.ok) {
+                el.innerHTML = await response.text();
+            } else {
+                console.error(`Błąd wczytywania pliku: ${file}`);
+            }
+        } catch (error) {
+            console.error(`Błąd: ${error}`);
+        }
+    });
+});
 
